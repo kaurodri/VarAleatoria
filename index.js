33a939bd4b0480073fbf61873269efad8da13021
HieA1lpha <engkauanrodrigues@gmail.com>
2023-04-09 23:28:08 -0300

    2023-04-09
    Criado um sistema que puxa elementos aleatoriamente dentro de uma Array.

---
+++ index.js
@@ -0,0 +1,6 @@
+var coisas = ["Elemento1", "Elemento2", "Elemento3", "Elemento4", "Elemento5", "Elemento6", "Elemento7"];
+
+let indices = coisas.length;
+let random = Math.floor(Math.random() * indices);
+
+console.log(coisas[random]);
